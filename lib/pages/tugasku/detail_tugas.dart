f5d63e31ffe171c4cfcfb0d2a1d2b92bb50bc105
Nitaa1904 <nitafitrotul1904@gmail.com>
2024-12-31 12:57:54 +0700

    fix: temporary fix for edit task feature (not fully resolved yet)

--- lib/pages/tugasku/detail_tugas.dart
+++ lib/pages/tugasku/detail_tugas.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:notequ/design_system/styles/color.dart';
 
 class DetailTugas extends StatefulWidget {
@@ -21,58 +22,185 @@ class DetailTugas extends StatefulWidget {
 
 class _DetailTugasState extends State<DetailTugas> {
   void _editTask() {
-    TextEditingController titleController =
-        TextEditingController(text: widget.task['title']);
-    TextEditingController noteController =
-        TextEditingController(text: widget.task['note']);
+    final _taskController = TextEditingController(text: widget.task['title']);
+    final _noteController = TextEditingController(text: widget.task['note']);
+    DateTime? selectedDate = widget.task['date'] != null
+        ? DateFormat('yyyy-MM-dd').parse(widget.task['date']!)
+        : null;
+    TimeOfDay? selectedTime = widget.task['time'] != null
+        ? TimeOfDay(
+            hour: int.parse(widget.task['time']!.split(":")[0]),
+            minute: int.parse(widget.task['time']!.split(":")[1]))
+        : null;
+    String selectedCategory = widget.task['category'] ?? '';
 
     showDialog(
       context: context,
       builder: (context) {
-        return AlertDialog(
-          title: const Text('Edit Tugas'),
-          content: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextField(
-                  controller: titleController,
-                  decoration: const InputDecoration(
-                    labelText: 'Judul',
-                    border: OutlineInputBorder(),
-                  ),
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+              title: const Text(
+                'Edit Tugas',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              content: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextFormField(
+                      controller: _taskController,
+                      decoration: InputDecoration(
+                        hintText: 'Masukkan judul tugas',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Text(
+                      'Pilih Kategori',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 8),
+                    Wrap(
+                      spacing: 8.0,
+                      runSpacing: 8.0,
+                      children:
+                          ['Pribadi', 'Pekerjaan', 'Belajar'].map((category) {
+                        return ChoiceChip(
+                          label: Text(category),
+                          selected: selectedCategory == category,
+                          selectedColor: Colors.blue.shade100,
+                          backgroundColor: Colors.grey.shade200,
+                          onSelected: (isSelected) {
+                            if (isSelected) {
+                              setState(() {
+                                selectedCategory = category;
+                              });
+                            }
+                          },
+                        );
+                      }).toList(),
+                    ),
+                    const SizedBox(height: 16),
+                    TextFormField(
+                      readOnly: true,
+                      onTap: () async {
+                        DateTime? pickedDate = await showDatePicker(
+                          context: context,
+                          initialDate: selectedDate ?? DateTime.now(),
+                          firstDate: DateTime(2000),
+                          lastDate: DateTime(2100),
+                        );
+                        if (pickedDate != null) {
+                          setState(() {
+                            selectedDate = pickedDate;
+                          });
+                        }
+                      },
+                      decoration: InputDecoration(
+                        hintText: selectedDate != null
+                            ? DateFormat('yyyy-MM-dd').format(selectedDate!)
+                            : 'Pilih tanggal buat tugas kamu',
+                        suffixIcon: const Icon(Icons.calendar_today),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    TextFormField(
+                      readOnly: true,
+                      onTap: () async {
+                        TimeOfDay? pickedTime = await showTimePicker(
+                          context: context,
+                          initialTime: selectedTime ?? TimeOfDay.now(),
+                        );
+                        if (pickedTime != null) {
+                          setState(() {
+                            selectedTime = pickedTime;
+                          });
+                        }
+                      },
+                      decoration: InputDecoration(
+                        hintText: selectedTime != null
+                            ? selectedTime!.format(context)
+                            : 'Pilih waktu buat tugas kamu',
+                        suffixIcon: const Icon(Icons.access_time),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Catatan',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        TextFormField(
+                          controller: _noteController,
+                          maxLines: 3,
+                          decoration: InputDecoration(
+                            hintText: 'Tambahkan catatan untuk tugas ini',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-                const SizedBox(height: 16),
-                TextField(
-                  controller: noteController,
-                  decoration: const InputDecoration(
-                    labelText: 'Catatan',
-                    border: OutlineInputBorder(),
-                  ),
-                  maxLines: 3,
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('Batal'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Pastikan waktu valid sebelum diolah
+                    if (selectedTime != null) {
+                      final formattedTime = TimeOfDay(
+                          hour: selectedTime!.hour,
+                          minute: selectedTime!.minute);
+
+                      widget.onTaskUpdated({
+                        'title': _taskController.text,
+                        'category': selectedCategory,
+                        'date': selectedDate != null
+                            ? DateFormat('yyyy-MM-dd').format(selectedDate!)
+                            : 'Tanggal belum dipilih',
+                        'time':
+                            '${formattedTime.hour}:${formattedTime.minute.toString().padLeft(2, '0')}',
+                        'note': _noteController.text.isNotEmpty
+                            ? _noteController.text
+                            : 'Tidak ada catatan',
+                      });
+                      Navigator.of(context).pop();
+                    } else {
+                      print(
+                          "Waktu pengingat tidak valid."); // Tambahkan log jika waktu tidak valid
+                    }
+                  },
+                  child: const Text('Simpan'),
                 ),
               ],
-            ),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Batal'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                widget.onTaskUpdated({
-                  ...widget.task,
-                  'title': titleController.text,
-                  'note': noteController.text,
-                });
-                Navigator.of(context).pop();
-              },
-              child: const Text('Simpan'),
-            ),
-          ],
+            );
+          },
         );
       },
     );
